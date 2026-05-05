c22c15c04de64be75aa8acf10afad678799d5517
nuentaru <gakeont001@gmail.com>
2026-05-06 00:05:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -657,27 +657,6 @@ app.add_error_handler(error_handler)
 
 
 logging.info("🚀 Bot đang chạy...")
-
-
-from flask import Flask
-import threading
-
-app_web = Flask(__name__)
-
-@app_web.route("/")
-def home():
-    return "Bot is running!"
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    app_web.run(host="0.0.0.0", port=port)
-
-def run_bot():
-    app.run_polling()
-
 if __name__ == "__main__":
-    t1 = threading.Thread(target=run_bot)
-    t1.daemon = True
-    t1.start()
-
-    run_web()
+    logging.info("🚀 Bot đang chạy...")
+    app.run_polling()
